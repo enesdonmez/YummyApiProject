5b58f1e9fe104aacba9cc1f45f3ef980a44e1da4
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-02-21 20:35:27 +0300

    Add FeaturesController and MessagesController classes
    - Added FeaturesController with methods for CRUD operations on features.
    - Added MessagesController with methods for CRUD operations on messages.

---
+++ Yummy.Api/Controllers/FeaturesController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Yummy.Api.Context;
+using Yummy.Api.Dtos.FeatureDtos;
+using Yummy.Api.Entitites;
+
+namespace Yummy.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ApiContext _context;
+
+        public FeaturesController(IMapper mapper, ApiContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _context.Features.ToList();
+            return Ok(_mapper.Map<List<ResulFeatureDto>>(values));
+        }
+
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDto createFeatureDto)
+        {
+            var feature = _mapper.Map<Feature>(createFeatureDto);
+            _context.Features.Add(feature);
+            _context.SaveChanges();
+            return StatusCode(201);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            _context.Features.Remove(value);
+            _context.SaveChanges();
+            return Ok("Feature silindi");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            _mapper.Map<GetByIdFeatureDto>(value);
+            return Ok(value);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDto updateFeatureDto)
+        {
+            var feature = _mapper.Map<Feature>(updateFeatureDto);
+            _context.Features.Update(feature);
+            _context.SaveChanges();
+            return Ok("Feature güncellendi");
+        }
+
+    }
+}
---
+++ Yummy.Api/Controllers/MessagesController.cs
@@ -0,0 +1,63 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Yummy.Api.Context;
+using Yummy.Api.Dtos.MessageDtos;
+using Yummy.Api.Entitites;
+
+namespace Yummy.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MessagesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        private readonly IMapper _mapper;
+
+        public MessagesController(ApiContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult MessageList()
+        {
+            var values = _context.Messages.ToList();
+            return Ok(_mapper.Map<List<Message>>(values));
+        }
+
+        [HttpPost]
+        public IActionResult CreateMessage(CreateMessageDto createMessageDto)
+        {
+            var value = _mapper.Map<Message>(createMessageDto);
+            _context.Messages.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMessage(int id)
+        {
+            var value = _context.Messages.Find(id);
+            return Ok(_mapper.Map<GetByIdMessageDto>(value));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMessage(int id)
+        {
+            var value = _context.Messages.Find(id);
+            _context.Messages.Remove(value);
+            _context.SaveChanges();
+            return Ok("Mesaj silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateMessage(UpdateMessageDto updateMessageDto)
+        {
+            var value = _mapper.Map<Message>(updateMessageDto);
+            _context.Messages.Update(value);
+            _context.SaveChanges();
+            return Ok("Mesaj güncellendi");
+        }
+    }
+}
