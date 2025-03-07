6163e926aacfa1da9e18dae24522751c60acfa71
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-03-07 14:21:04 +0300

    Add MenuItemMapping for DTOs using AutoMapper

---
+++ Yummy.Api/Mapping/MenuItemMapping.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Yummy.Api.Dtos.MenuItemDtos;
+using Yummy.Api.Entitites;
+
+namespace Yummy.Api.Mapping;
+
+public class MenuItemMapping : Profile
+{
+    public MenuItemMapping()
+    {
+        CreateMap<MenuItem, ResultMenuItemDto>().ReverseMap();
+        CreateMap<MenuItem, CreateMenuItemDto>().ReverseMap();
+        CreateMap<MenuItem, GetByIdMenuItemDto>().ReverseMap();
+        CreateMap<MenuItem, UpdateMenuItemDto>().ReverseMap();
+    }
+}
