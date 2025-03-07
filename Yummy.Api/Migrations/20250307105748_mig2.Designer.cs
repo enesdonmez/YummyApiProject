98a4ffbe0301dd95600220c28969363c69767246
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-03-07 14:20:10 +0300

    Add migration for MenuItems and Categories relationship

--- Yummy.Api/Migrations/20250307105748_mig2.Designer.cs
+++ Yummy.Api/Migrations/20250307105748_mig2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Yummy.Api.Context;
 
@@ -11,9 +12,11 @@ using Yummy.Api.Context;
 namespace Yummy.Api.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    partial class ApiContextModelSnapshot : ModelSnapshot
+    [Migration("20250307105748_mig2")]
+    partial class mig2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Yummy.Api/Migrations/20250307105748_mig2.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Yummy.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "MenuItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuItems_CategoryId",
+                table: "MenuItems",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MenuItems_Categories_CategoryId",
+                table: "MenuItems",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_MenuItems_Categories_CategoryId",
+                table: "MenuItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MenuItems_CategoryId",
+                table: "MenuItems");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "MenuItems");
+        }
+    }
+}
