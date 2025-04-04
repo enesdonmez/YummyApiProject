67b7da09547c6b7956f2fa7fbb957eaf56b7ad08
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-04-04 22:00:47 +0300

    add image dtos

---
+++ Yummy.Api/Dtos/ImageDtos/CreateImageDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Yummy.Api.Dtos.ImageDtos
+{
+    public class CreateImageDto
+    {
+        public string Title { get; set; } = default!;
+        public string ImageUrl { get; set; } = default!;
+    }
+}
---
+++ Yummy.Api/Dtos/ImageDtos/GetImagetByIdDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Yummy.Api.Dtos.ImageDtos
+{
+    public class GetImagetByIdDto
+    {
+        public int ImageId { get; set; }
+        public string Title { get; set; } = default!;
+        public string ImageUrl { get; set; } = default!;
+    }
+}
