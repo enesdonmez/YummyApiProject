f241425c459409113027f4150c0879f2cc679021
Enes DÖNMEZ <enesd_5034@outlook.com>
2025-05-28 01:03:38 +0300

    add project files

---
+++ Yummy.AdminUI/ViewComponents/_NavbarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yummy.AdminUI.ViewComponents
+{
+    public class _NavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Yummy.AdminUI/ViewComponents/_SettingsSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yummy.AdminUI.ViewComponents
+{
+    public class _SettingsSidebarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
